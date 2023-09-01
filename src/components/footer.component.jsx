b4d35afe629eb0bdbaacf37d4352f23d7266e133
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-09-01 16:27:57 +0700

    update footer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FooterComponent = () => {
   return (
     <footer className="text-center text-white" style={{ backgroundColor: '#f1f1f1' }}>
-      <div className="text-center text-dark p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div className="text-center text-dark p-3" style={{ backgroundColor: '#F8F9FA' }}>
         © 2023 Copyright Adrian Mulyawan With Love
       </div>
     </footer>
