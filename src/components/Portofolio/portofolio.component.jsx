54157e242d38d28f600f5f6ce3ede9607848a164
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-09-01 20:37:04 +0700

    update

--- src/components/Portofolio/portofolio.component.jsx
+++ src/components/Portofolio/portofolio.component.jsx
@@ -9,14 +9,14 @@ const PortofolioComponent = () => {
         PORTOFOLIO
       </h1>
       <div className="row justify-content-center align-items-center py-sm-0 py-md-0 py-lg-2">
-        <CardPortofolioComponent image = { portoImage } project = 'Website Desa Kuala Secapah' url = 'https://kualasecapah.my.id/'  />
-        <CardPortofolioComponent image = { portoImage } project = 'React Express JS Games Web' url = 'https://github.com/FSW-29/challenge-chapter-11'  />
-        <CardPortofolioComponent image = { portoImage } project = 'Next JS Games App' url = 'https://github.com/FSW-29/ChallengeChapter10' />
-        <CardPortofolioComponent image = { portoImage } project = 'React JS Games App' url = 'https://github.com/FSW-29/ChallengeChapter9' />
-        <CardPortofolioComponent image = { portoImage } project = 'Food Receipe App' url = 'https://react-food-recipe-seven.vercel.app/' />
-        <CardPortofolioComponent image = { portoImage } project = 'SIM Kawasan Konservasi' url = 'https://github.com/adrianmulyawan/salam-konservasi-be' />
-        <CardPortofolioComponent image = { portoImage } project = 'Travel Website' url = 'https://github.com/adrianmulyawan/fullstack-travel' />
-        <CardPortofolioComponent image = { portoImage } project = 'Bootcamp Website' url = 'https://github.com/adrianmulyawan/laracamp' />
+        <CardPortofolioComponent image = { portoImage } project = 'Website Desa Kuala Secapah' url = 'https://kualasecapah.my.id/'/>
+        <CardPortofolioComponent image = { portoImage } project = 'React Express JS Games Web' url = 'https://github.com/FSW-29/challenge-chapter-11'/>
+        <CardPortofolioComponent image = { portoImage } project = 'Next JS Games App' url = 'https://github.com/FSW-29/ChallengeChapter10'/>
+        <CardPortofolioComponent image = { portoImage } project = 'React JS Games App' url = 'https://github.com/FSW-29/ChallengeChapter9'/>
+        <CardPortofolioComponent image = { portoImage } project = 'Food Receipe App' url = 'https://react-food-recipe-seven.vercel.app/'/>
+        <CardPortofolioComponent image = { portoImage } project = 'SIM Kawasan Konservasi' url = 'https://github.com/adrianmulyawan/salam-konservasi-be'/>
+        <CardPortofolioComponent image = { portoImage } project = 'Travel Website' url = 'https://github.com/adrianmulyawan/fullstack-travel'/>
+        <CardPortofolioComponent image = { portoImage } project = 'Bootcamp Website' url = 'https://github.com/adrianmulyawan/laracamp'/>
       </div>
     </div>
   );
