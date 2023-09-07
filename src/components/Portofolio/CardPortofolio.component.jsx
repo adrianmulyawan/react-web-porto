1c2d3b3dd01547c9372676ef007ba93a997cba23
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-09-07 13:37:18 +0700

    edit portofolio

--- src/components/Portofolio/CardPortofolio.component.jsx
+++ src/components/Portofolio/CardPortofolio.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CardPortofolioComponent = (props) => {
   const { image, project, url } = props;
+
+  const goProject = () => {
+    window.open(url);
+  };
 
   return (
     <div className="col-sm-12 col-md-6 col-lg-3">
@@ -9,7 +14,7 @@ const CardPortofolioComponent = (props) => {
         <img className="card-img-top" src={ image } alt="work-experience" />
         <div className="card-body">
           <h5 className='card-title' style={{ color: 'navy', fontSize: '18px' }}>{ project }</h5>
-          <a href={ url } className='btn bg-warning p-2 my-2' target='_blank' rel='noreferrer' style={{ fontWeight: 'bold', fontSize: '12px' }}>Show Project</a>
+          <Link onClick={ goProject } className='btn bg-warning p-2 my-2' target='_blank' rel='noreferrer' style={{ fontWeight: 'bold', fontSize: '12px' }}>Show Project</Link>
         </div>
       </div>
     </div>
