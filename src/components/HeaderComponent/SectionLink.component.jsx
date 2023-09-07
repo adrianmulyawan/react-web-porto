aba66b87994a40f6c58c3babefd6e51c59720d52
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-09-07 13:00:36 +0700

    edit header

--- src/components/HeaderComponent/SectionLink.component.jsx
+++ src/components/HeaderComponent/SectionLink.component.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SectionLinkComponent = (props) => {
   const { icon, link } = props;
 
+  const toLink = () => {
+    window.open(link);
+  }
+
   return (
     <>
       <div className="col-3">
-        <a href={ link } target="_blank" rel="noopener noreferrer">
+        <Link onClick={ toLink } target="_blank" rel="noopener noreferrer">
           <img src={icon} alt="link-me" style={{ width: '40px' }} />
-        </a>
+        </Link>
       </div>
     </>
   );
